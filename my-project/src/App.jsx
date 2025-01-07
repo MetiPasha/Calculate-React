c3722b281aedd316f5568413ef862a12cf97a2a8
pasha <mtipasha2021@gmail.com>
2025-01-07 15:28:37 +0330

    in app.jsx we put buttons

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,7 +3,23 @@ import "./index.css";
 function App() {
   return (
     <div>
-      <div></div>
+      <button>AC</button>
+      <button>DEl</button>
+      <button>&divide;</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
     </div>
   );
 }
